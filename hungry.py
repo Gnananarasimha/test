1545df61037d906f85419319e26fa67a641db305
Gnananarasimha <gnananarasimha18@gmail.com>
2024-02-05 11:42:47 +0530

    second version of hungry.py code

--- hungry.py
+++ hungry.py
@@ -1,1 +1,6 @@
-print("I'm hungry")
+hungry=input("Are you feeling hungry? ")
+hungry=hungry.lower()
+if hungry == "yes":
+    print("Take some snacks and come back to work")
+else:
+    print("Do your work...")
